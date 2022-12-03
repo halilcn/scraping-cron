bc0724528bd0cae3094d8cc300ecc8cba28130c3
Halil <iletisimhalilcan@gmail.com>
2022-12-03 20:46:10 +0300

    update axios headers for 503 error

--- src/utils/getPage.ts
+++ src/utils/getPage.ts
@@ -4,7 +4,16 @@ import { TooManyRequestError, UnknownError } from './errors'
 const getPage = async (url: string) => {
   try {
     const headers = {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0',
+      Accept: 'application/json, text/plain, */*',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'tur',
+      refer: 'https://www.hepsiemlak.com/',
+      'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+      'sec-ch-ua-platform': 'macOS',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-origin',
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
     }
     const { data } = await axios.get(url, { headers })
     return data
