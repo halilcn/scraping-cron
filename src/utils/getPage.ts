8fbe157031f14f553899f5c5ca201904b2edf4f5
Halil <iletisimhalilcan@gmail.com>
2022-12-03 20:35:24 +0300

    add user agent header for axios

--- src/utils/getPage.ts
+++ src/utils/getPage.ts
@@ -3,7 +3,10 @@ import { TooManyRequestError, UnknownError } from './errors'
 
 const getPage = async (url: string) => {
   try {
-    const { data } = await axios.get(url)
+    const headers = {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0',
+    }
+    const { data } = await axios.get(url, { headers })
     return data
   } catch (err: any) {
     if (err?.response?.status === 429) throw new TooManyRequestError()
